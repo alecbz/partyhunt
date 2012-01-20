819459b1606c9ae25a02e8d75c90cddf9c14088b
Alec Benzer <alecbenzer@gmail.com>
2012-01-20 01:54:03 -0600

    added password hashing/salts, made a User.register method

--- app.rb
+++ app.rb
@@ -15,29 +15,46 @@ class User
       :is_unique => "That username is already taken"
     }
   property :password, String
+  property :salt, String
   property :created, DateTime
+
+  def self.register(username,password)
+    salt = random_salt
+    hash = hash_password(password,salt)
+    User.new(:username => username,
+        :password => hash,
+        :salt => salt,
+        :created => Time.now)
+  end
 end
 
 DataMapper.finalize
 DataMapper.auto_migrate!
+
+def random_salt
+  letters = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+  n = letters.size
+  (0...10).map{ letters[rand(n)] }.join
+end
+
+def hash_password(password,salt)
+  Digest::SHA1.hexdigest(password+salt)
+end
 
 get '/' do
   erb :index
 end
 
 post '/register' do
-  # do stuff
-  @user = User.new(:username => params[:username],
-              :password => params[:password],
-              :created => Time.now)
+  @user = User.register(params[:username],params[:password])
   if @user.save 
-    redirect "/user/#{@user.id}"
+    redirect "/user/#{@user.username}"
   else
     erb :index
   end
 end
 
-get '/user/:id' do |id|
-  @user = User.get(id)
+get '/user/:username' do |username|
+  @user = User.first(:username => params[:username])
   erb :user
 end
